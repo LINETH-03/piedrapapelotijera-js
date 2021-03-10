681d3172788873b8b6ef65d182d7bb5ff49f3d3f
LINETH-03 <lperezh9@miumg.edu.gt>
2021-03-09 23:29:43 -0600

    funcionalidad

--- program.js
+++ program.js
@@ -1,7 +1,22 @@
-let j1 = new Array["PIEDRA", "PAPEL","TIJERA"];
-let j2 = new Array["PIEDRA", "PAPEL","TIJERA"];
-var aciertos =0;
-var errores =0;
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
 
-Math.floor(Math.random()*j1.length);
+const player1 = ["Papel", "Piedra", "Tijera"];
+const jug1 = Math.floor(Math.random() * player1.length);
+console.log("Jugador 1 sacó:  "+player1[jug1]);
+const player2 = ["Papel", "Piedra", "Tijera"];
+const jug2 = Math.floor(Math.random() * player2.length);
+console.log("Jugador 2 sacó:  "+player2[jug2]);
+
+if(jug1==jug2){
+    
+   console.log("Es un empate")
+}
+else if(jug1==["0"] && jug2==["1"] || jug1==["1"] && jug2==["2"]|| jug1==["2"] && jug2==["0"]){
+    
+    console.log("GANA jugador 1 gana a jugador 2 " );
+}else{
+    console.log("GANA jugador 2 gana a jugador 1 " );
+}
+
 
